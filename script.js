efdea16b8940db4cfaa7ddbf884d4f259a76fa86
RobertVLevesque <robertvlevesque@gmail.com>
2025-04-07 07:59:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,31 @@ fetch('https://unpkg.com/world-atlas/countries-110m.json')
   .then(res => res.json())
   .then(countries => {
     const world = Globe()
-      (document.getElementById('globeViz'))
-      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-      .polygonsData(
-        window.topojson.feature(countries, countries.objects.countries).features
-      )
-      .polygonAltitude(0.01)
-      .polygonCapColor(() => 'rgba(255, 100, 100, 0.7)')
-      .polygonSideColor(() => 'rgba(0, 100, 255, 0.15)')
-      .polygonStrokeColor(() => '#111')
+    Globe()
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+  .backgroundColor('#0d0d0d') // deep space black
+  .globeMaterial(new THREE.MeshPhongMaterial({
+    color: 0x111111,
+    emissive: 0x222222,
+    shininess: 15,
+    transparent: true,
+    opacity: 1
+  }))
+  .polygonCapColor(() => '#00ffff') // neon cyan
+  .polygonSideColor(() => '#ff00ff') // magenta side glow
+  .polygonStrokeColor(() => '#ff00ff') // borders
+  .polygonLabel(({ NAME }) => `
+    <div style="color: #fff; font-family: 'Orbitron', sans-serif;">
+      <strong style="color:#00ffff;">🌐 ${NAME}</strong><br/>
+      <em style="color:#ff00ff;">Artist: TBD</em><br/>
+      <span style="color:#ffffff;">"This is a placeholder story."</span>
+    </div>
+  `)
+  .polygonAltitude(0.04)
+  .showAtmosphere(true)
+  .atmosphereColor('#ff00ff')
+  .atmosphereAltitude(0.15);
+
       .onPolygonClick(feat => {
         const popup = document.getElementById('popup');
         const name = feat.properties.name;
